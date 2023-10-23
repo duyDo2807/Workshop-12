e44b53ad59d50f6727a972195197974cad8a1e37
duyDo2807 <a1881078@adelaide.edu.au>
2023-10-24 02:08:31 +1030

    Workshop 12

--- main-3.cpp
+++ main-3.cpp
@@ -3,27 +3,27 @@
 #include "Game.h"
 
 int main() {
-  Game game;
-  std::vector<GameEntity*> entities = game.initGame(3, 3, 10, 10);
+  Game myGame;
+  std::vector<GameEntity*> entities = myGame.getEntities();
 
   std::cout << "Initial Game Entities:\n";
-  for (GameEntity* entity : entities) {
+  for (auto entity : entities) {
     std::cout << "Type: " << entity->getType() << ", Position: ("
               << std::get<0>(entity->getPos()) << ", "
               << std::get<1>(entity->getPos()) << ")\n";
   }
 
-  game.gameLoop(5, 2.0);
+  myGame.gameLoop(5, 2.0);
 
   std::cout << "Game Entities after Simulation:\n";
-  for (GameEntity* entity : game.getEntities()) {
+  for (auto entity : myGame.getEntities()) {
     std::cout << "Type: " << entity->getType() << ", Position: ("
               << std::get<0>(entity->getPos()) << ", "
               << std::get<1>(entity->getPos()) << ")\n";
   }
 
   // Clean up the entities to avoid memory leaks
-  for (GameEntity* entity : entities) {
+  for (auto entity : entities) {
     delete entity;
   }
 
